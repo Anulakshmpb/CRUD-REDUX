78ca5c6205967977dbf0a0e074c7eb656f6c5d69
Anulakshmpb <anulakshmipb2407@gmail.com>
2025-11-27 22:21:07 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Navbar from './Components/Navbar';
 import Create from './Components/Create';
 import { BrowserRouter, Route, Router, Routes } from 'react-router-dom';
+import Read from './Components/Read';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Navbar/>
       <Routes>
         <Route exact path='/' element={<Create/>}></Route>
+        <Route exact path='/read' element={<Read/>}></Route>
       </Routes>
      
       </BrowserRouter>
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,27 +1,28 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Navbar() {
   return (
 	<div>
-		<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <div class="container-fluid">
-    <a class="navbar-brand fs-2 fw-bolder" href="#">RTK</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
+		<nav className="navbar navbar-expand-lg navbar-light bg-light">
+  <div className="container-fluid">
+    <h4 className="navbar-brand fs-2 fw-bolder">RTK</h4>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Create Post</a>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <li className="nav-item">
+          <Link to="/" className="nav-link active">Create Post</Link>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">All Post</a>
+        <li className="nav-item">
+          <Link to="/read" className="nav-link">All Post</Link>
         </li>
         
       </ul>
-      <form class="d-flex">
-        <input class="form-control me-2 " type="search" placeholder="Search" aria-label="Search"></input>
-        <button class="btn btn-outline-success" type="submit">Search</button>
+      <form className="d-flex">
+        <input className="form-control me-2 " type="search" placeholder="Search" aria-label="Search"></input>
+        <button className="btn btn-outline-success" type="submit">Search</button>
       </form>
     </div>
   </div>
